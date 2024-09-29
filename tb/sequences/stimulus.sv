9c27150aa91bc9f551a9d52fbc17d1f794375fce
Fabian Picado <fpg32199@gmail.com>
2024-09-29 10:03:30 -0600

    Added constraint for build valid RISC-V instructions

--- tb/sequences/stimulus.sv
+++ tb/sequences/stimulus.sv
@@ -12,5 +12,26 @@ class stimulus;
   rand logic [31:0] riscv_inst;
   rand logic [31:0] riscv_data;
 
+  constraint valid_inst {
+    if (inst_type == r_type) {
+      riscv_inst == {funct7, rs2, rs1, funct3, rd, opcode};
+    }
+    else if (inst_type == i_type) {
+      riscv_inst == {imm[11:0], rs1, funct3, rd, opcode};
+    }
+    else if (inst_type == s_type) {
+      riscv_inst == {imm[11:5], rs2, rs1, funct3, imm[4:0], opcode};
+    }
+    else if (inst_type == b_type) {
+      riscv_inst == {imm[12], imm[10:5], rs2, rs1, funct3, imm[4:1], imm[11], opcode};
+    }
+    else if (inst_type == u_type) {
+      riscv_inst == {imm[31:12], rd, opcode};
+    }
+    else { // Assume that it will be a J instruction type
+      riscv_inst == {imm[20], imm[10:1], imm[11], imm[19:12], rd, opcode};
+    }
+    solve inst_type before riscv_inst; // We need to first known the instruction type in order to determine valid instructions
+  }
   
 endclass : stimulus
