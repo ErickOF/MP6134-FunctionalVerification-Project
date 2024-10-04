3221016b22fc5e7862737935bac6b4e850f612ba
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2024-10-03 19:17:33 -0600

    Updating monitor with basic checkers for instruction matching

---
+++ tb/environment/helper.sv
@@ -0,0 +1,19 @@
+// ANSI escape codes for colors
+// -- Normal color
+`define COLOR_RESET "\033[0m"
+// -- Green color
+`define COLOR_INFO "\033[32m"
+// -- Yellow color
+`define COLOR_WARNING "\033[33m"
+// -- Red color
+`define COLOR_ERROR "\033[31m"
+// -- Magenta color
+`define COLOR_FATAL "\033[35m"
+
+`define PRINT(COLOR, ID_MSG, TYPE, MSG) \
+  $display("%s%s %s (%0d) @ %0t: report [%s] %s%s", COLOR, TYPE, `__FILE__, `__LINE__, $time, ID_MSG, MSG, `COLOR_RESET);
+
+`define PRINT_INFO(ID_MSG, MSG) `PRINT(`COLOR_INFO, ID_MSG, "INFO", MSG)
+`define PRINT_WARNING(ID_MSG, MSG) `PRINT(`COLOR_WARNING, ID_MSG, "WARNING", MSG)
+`define PRINT_ERROR(ID_MSG, MSG) `PRINT(`COLOR_ERROR, ID_MSG, "ERROR", MSG)
+`define PRINT_FATAL(ID_MSG, MSG) `PRINT(`COLOR_FATAL, ID_MSG, "FATAL", MSG) $fatal;
--- tb/environment/monitor.sv
+++ tb/environment/monitor.sv
@@ -1,10 +1,24 @@
 // Based on: https://www.edaplayground.com/x/Yk4N
+`include "helper.sv"
+
+`define MONITOR_NAME "MONITOR"
+
 class monitor;
   // Reference to the scoreboard to fetch expected values
   scoreboard sb;
 
   // Virtual interface to observe signals from the DUT
   virtual darkriscv_if intf;
+
+  // Instruction and data from scoreboard and interface
+  logic [31:0] instruction_sb;
+  logic [31:0] instruction_intf;
+  logic [31:0] data_sb;
+  logic [31:0] data_inft;
+
+  // PASS/ERROR counters
+  int pass_counter;
+  int error_counter;
 
   // Constructor: Initializes the interface and scoreboard objects
   function new(virtual darkriscv_if intf, scoreboard sb);
@@ -14,5 +28,32 @@ class monitor;
 
   // Check task: Continuously monitors the read enable signal and checks the data output
   task check();
+    `PRINT_INFO(`MONITOR_NAME, "Check task running")
+    pass_counter = 0;
+    error_counter = 0;
+
+    forever begin
+      // Check on clock posedge
+      @(negedge intf.CLK);
+
+      if ((intf.RES === 0) && (sb.instruction_queue.size() > 0)) begin
+        instruction_sb = sb.instruction_queue.pop_back();
+	instruction_intf = intf.IDATA;
+
+	`PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction from Scoreboard: 0x%0h", instruction_sb))
+	`PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction from Interface: 0x%0h", instruction_intf))
+
+	if (instruction_sb === instruction_intf) begin
+	  `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface match")
+	  pass_counter++;
+	end
+	else begin
+	  `PRINT_ERROR(`MONITOR_NAME, "Scoreboard and interface mismatch")
+	  error_counter++;
+	end
+
+        $display();
+      end
+    end
   endtask : check
 endclass : monitor
