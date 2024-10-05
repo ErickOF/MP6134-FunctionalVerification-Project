ecbfd6cfc4f317dded30f8fc70720551552b607d
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2024-10-05 04:20:47 -0600

    Adding code documentation for monitor and instructions_pkg

--- tb/environment/instructions_pkg.svh
+++ tb/environment/instructions_pkg.svh
@@ -1,32 +1,105 @@
+//#################################################################################################
+// File: instructions_pkg.svh
+// Description: This package defines an enumeration for RISC-V instruction types, as well as data
+//              structures representing instruction formats (specifically `i_type_t`
+//              for immediate-type instructions) and a union `instruction_t` that can hold
+//              different instruction types.
+//
+//              The package is designed for RISC-V ISA instruction encoding with a focus on opcode
+//              and immediate-type instructions.
+//##############################################################################
+
+// Guard to prevent multiple inclusions of this file
 `ifndef INSTRUCTIONS_PKG_SVH
 `define INSTRUCTIONS_PKG_SVH
 
+//#################################################################################################
+// Package: instructions_pkg
+// This package contains enumerations and structures to represent RISC-V instruction types and
+// formats.
+//#################################################################################################
+
 package instructions_pkg;
+  //###############################################################################################
+  // Enum: inst_type_e
+  // Description: Defines the opcode field for different types of RISC-V instructions. Each type is
+  //              represented by a 7-bit value.
+  //
+  // Fields:
+  //  - r_type: Represents the opcode for R-type instructions (register type).
+  //  - i_type: Represents the opcode for I-type instructions (immediate type).
+  //  - s_type: Represents the opcode for S-type instructions (store type).
+  //  - b_type: Represents the opcode for B-type instructions (branch type).
+  //  - u_type: Represents the opcode for U-type instructions (upper immediate type).
+  //  - j_type: Represents the opcode for J-type instructions (jump type).
+  //##########################################################################
+  typedef enum logic [6:0] {
+    // R-type instructions (register)
+    r_type = 7'b011_0011,
+    // I-type instructions (immediate)
+    // TODO: only for: ADDI, XORI, ORI, ANDI, SLLI, SRLI, SRAI, SLTI and SLTIU
+    i_type = 7'b001_0011,
+    // S-type instructions (store)
+    s_type = 7'b010_0011,
+    // B-type instructions (branch)
+    b_type = 7'b110_0011,
+    // U-type instructions (AUIPC)
+    // TODO: only for AUIPC
+    u_type = 7'b001_0111,
+    // J-type instructions (JAL)
+    // TODO: only for JAL
+    j_type = 7'b110_1111
+  } inst_type_e;
 
-typedef enum logic [6:0] {
-  r_type = 7'b011_0011,
-  i_type = 7'b001_0011, //TODO: only for: ADDI, XORI, ORI, ANDI, SLLI, SRLI, SRAI, SLTI and SLTIU
-  s_type = 7'b010_0011,
-  b_type = 7'b110_0011,
-  u_type = 7'b001_0111, //TODO: only for AUIPC
-  j_type = 7'b110_1111 //TODO: only for JAL
-} inst_type_e;
-
-// Represents IMM instructions
+//#################################################################################################
+// Struct: i_type_t
+// Description: Represents an I-type (immediate-type) RISC-V instruction format. This structure
+//              encodes the components of an I-type instruction.
+//
+// Fields:
+//  - imm  : The immediate value, 12 bits.
+//  - rs1  : Source register 1, 5 bits.
+//  - func3: Function field to define the operation, 3 bits.
+//  - rd   : Destination register, 5 bits.
+//  - opcode: Opcode field from the `inst_type_e` enum, 7 bits.
+//
+// Example: A typical I-type instruction might be ADDI, which adds an immediate value to the value
+//          of rs1 and stores the result in rd.
+//#################################################################################################
 typedef struct packed {
+  // Immediate field (12 bits)
   logic [11:0] imm;
+  // Source register 1 (5 bits)
   logic [4:0] rs1;
+  // Function field (3 bits)
   logic [2:0] func3;
+  // Destination register (5 bits)
   logic [4:0] rd;
+  // Opcode (7 bits)
   inst_type_e opcode;
 } i_type_t;
 
-// Represents the RISV-V instruction
+//#################################################################################################
+// Union: instruction_t
+// Description: A union that represents a RISC-V instruction. It can either  hold the opcode field
+//              directly, or represent an I-type instruction.
+//
+// Fields:
+//  - opcode: A structure that contains the opcode (7 bits) and filler bits.
+//  - i_type: A structure representing an I-type instruction (12-bit immediate, rs1, func3, rd, and
+//            opcode).
+//
+// Example: This union can be used to interpret the 32-bit instruction word as either just an
+//          opcode or as a more complex I-type instruction.
+//#################################################################################################
 typedef union packed {
   struct packed {
+    // Filler bits to occupy space up to bit 31
     logic [31:7] fillin;
+    // Opcode field (7 bits)
     inst_type_e opcode;
   } opcode;
+  // I-type instruction
   i_type_t i_type;
 } instruction_t;
 
--- tb/environment/monitor.sv
+++ tb/environment/monitor.sv
@@ -1,8 +1,27 @@
-// Based on: https://www.edaplayground.com/x/Yk4N
 `define MONITOR_NAME "MONITOR"
 
+//#################################################################################################
+// Class: monitor
+// Description: This class monitors the instructions and data of the DUT and compares them with the
+//              expected values from  the scoreboard. It continuously checks signals on the virtual
+//              interface and ensures the DUT operates correctly by comparing the scoreboard's
+//              expected values with the DUT's actual output.
+//
+//              If mismatches occur, the monitor increments error counters apropriate messages.
+//
+//              It includes special checks for different instruction types (`r_type` and `i_type`).
+//
+// Parameters:
+//  - intf: The virtual interface connected to the DUT.
+//  - sb  : The scoreboard containing expected values for instructions and data.
+//#################################################################################################
 class monitor;
+  // Import instruction types from the package
   import instructions_pkg::*;
+
+  //###############################################################################################
+  // Members:
+  //###############################################################################################
 
   // Reference to the scoreboard to fetch expected values
   scoreboard sb;
@@ -11,85 +30,220 @@ class monitor;
   virtual darkriscv_if intf;
 
   // Instruction and data from scoreboard and interface
+  // Expected instruction from scoreboard
   instruction_t instruction_sb;
+  // Actual instruction from DUT interface
   instruction_t instruction_intf;
+  // Expected data from scoreboard
   logic [31:0] data_sb;
+  // Actual data from DUT interface
   logic [31:0] data_intf;
+  // Instruction opcode (enum)
   inst_type_e opcode;
 
-  // PASS/ERROR counters
+  // PASS/ERROR counters to track matching/mismatched comparisons
   int pass_counter;
   int error_counter;
 
-  // Constructor: Initializes the interface and scoreboard objects
+  //###############################################################################################
+  // Constructor: new
+  // Description: Initializes the monitor object with the virtual interface and scoreboard passed
+  //              as parameters.
+  //
+  // Parameters:
+  //  - intf: Virtual interface to observe signals from the DUT.
+  //  - sb  : Scoreboard reference to fetch expected instruction and data values.
+  //###############################################################################################
   function new(virtual darkriscv_if intf, scoreboard sb);
     this.intf = intf;
     this.sb = sb;
   endfunction : new
 
-  // Check task: Continuously monitors the read enable signal and checks the data output
+  //###############################################################################################
+  // Task: check
+  // Description: This task continuously monitors the DUT's signals on every negative edge of the
+  //              clock. It compares the instructions and data from the scoreboard with those from
+  //              the DUT.
+  // 
+  //              If there is a mismatch, the error counter is incremented, and an error message is
+  //              printed. Otherwise, a pass message is shown.
+  //
+  //              Additionally, it checks the type of instruction (e.g., `r_type` or `i_type`) and
+  //              calls specialized check functions for each type.
+  //###############################################################################################
   task check();
     `PRINT_INFO(`MONITOR_NAME, "Check task running")
+    
+    // Initialize counters
     pass_counter = 0;
     error_counter = 0;
 
+    // Infinite loop to continuously check the DUT
     forever begin
-      // Check on clock posedge
+      // Check on negative edge of the clock
       @(negedge intf.CLK);
 
+      // Ensure the reset is de-asserted and there are instructions in the scoreboard queue
       if ((intf.RES === 0) && (sb.instruction_queue.size() > 0)) begin
+        // Fetch the instruction and data from the scoreboard and DUT interface
         instruction_sb = sb.instruction_queue.pop_back();
-	instruction_intf = intf.IDATA;
+        instruction_intf = intf.IDATA;
         data_sb = sb.data_queue.pop_back();
         data_intf = intf.IDATA;
-	opcode = instruction_sb.opcode.opcode;
+        opcode = instruction_sb.opcode.opcode;
 
-	`PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction from Scoreboard: 0x%0h", instruction_sb))
-	`PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction from Interface: 0x%0h", instruction_intf))
+        // Print information about the fetched instructions
+        `PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction from Scoreboard: 0x%0h", instruction_sb))
+        `PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction from Interface: 0x%0h", instruction_intf))
 
-	if (instruction_sb === instruction_intf) begin
-	  `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface instructions match")
-	  pass_counter++;
-	end
-	else begin
-	  `PRINT_ERROR(`MONITOR_NAME, "Scoreboard and interface instructions mismatch")
-	  error_counter++;
-	end
-
-	// Check instruction type
-	`PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction of type \"%s\"", opcode.name()))
-
-	if (opcode == r_type) begin
-	  check_r_type();
+        // Compare instructions from the scoreboard and the DUT
+        if (instruction_sb === instruction_intf) begin
+          `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface instructions match")
+          pass_counter++;
         end
-	else if (opcode == i_type) begin
-	  check_i_type();
-	end
+        else begin
+          `PRINT_ERROR(`MONITOR_NAME, "Scoreboard and interface instructions mismatch")
+          error_counter++;
+        end
 
-	$display();
+        // Check the instruction type and call respective check functions
+        `PRINT_INFO(`MONITOR_NAME, $sformatf("Instruction of type \"%s\"", opcode.name()))
 
+        if (opcode == b_type) begin
+          // Specialized check for B-type instructions
+          check_b_type();
+        end
+        else if (opcode == i_type) begin
+          // Specialized check for I-type instructions
+          check_i_type();
+        end
+        else if (opcode == j_type) begin
+          // Specialized check for J-type instructions
+          check_j_type();
+        end
+        else if (opcode == r_type) begin
+          // Specialized check for R-type instructions
+          check_r_type();
+        end
+        else if (opcode == s_type) begin
+          // Specialized check for S-type instructions
+          check_s_type();
+        end
+        else if (opcode == u_type) begin
+          // Specialized check for U-type instructions
+          check_u_type();
+        end
+        else begin
+          `PRINT_WARNING(`MONITOR_NAME, $sformatf("Instruction type \"%s\" is not supported", opcode))
+        end
+
+        $display();
+
+        // Compare the data input from the scoreboard and the interface
         `PRINT_INFO(`MONITOR_NAME, $sformatf("Data input from Scoreboard: 0x%0h", data_sb))
-	`PRINT_INFO(`MONITOR_NAME, $sformatf("Data input from Interface: 0x%0h", data_intf))
+        `PRINT_INFO(`MONITOR_NAME, $sformatf("Data input from Interface: 0x%0h", data_intf))
 
-	if (data_sb == data_intf) begin
-	  `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface input data match")
-	  pass_counter++;
-	end
-	else begin
-	  `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface input data mismatch")
-	  error_counter++;
-	end
+        if (data_sb == data_intf) begin
+          `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface input data match")
+          pass_counter++;
+        end
+        else begin
+          `PRINT_INFO(`MONITOR_NAME, "Scoreboard and interface input data mismatch")
+          error_counter++;
+        end
 
         $display();
       end
     end
   endtask : check
 
+  //###############################################################################################
+  // Task: check_b_type
+  // Description: Specialized check for B-type instructions. This task is called when a B-type
+  //              instruction is detected.
+  //
+  //              B-type instructions include conditional branches such as BEQ (branch if equal),
+  //              BNE (branch if not equal), etc.
+  // 
+  // Example: B-type instructions are used for conditional branching based on register comparisons
+  //          in RISC-V architectures.
+  //
+  //###############################################################################################
+  task check_b_type();
+    `PRINT_INFO(`MONITOR_NAME, "Checking \"b_type\" instruction")
+  endtask : check_b_type
+
+  //###############################################################################################
+  // Task: check_i_type
+  // Description: Specialized check for I-type instructions. This task is called when an I-type
+  //              instruction is detected.
+  //
+  // Example: I-type instructions include ADDI, XORI, ORI, etc.
+  //###############################################################################################
   task check_i_type();
     `PRINT_INFO(`MONITOR_NAME, "Checking \"i_type\" instruction")
   endtask : check_i_type
 
+  //###############################################################################################
+  // Task: check_j_type
+  // Description: Specialized check for J-type instructions. This task is called when a J-type
+  //              instruction is detected.
+  //
+  //              J-type instructions are used for jump operations, which typically alter the flow
+  //              of execution by jumping to a specified address. The most common J-type
+  //              instruction in RISC-V is JAL (Jump and Link), which jumps to a target address and
+  //              saves the return address in a register.
+  //
+  // Example: J-type instructions can be utilized for function calls and for implementing control
+  //          flow in programs.
+  //
+  //###############################################################################################
+  task check_j_type();
+    `PRINT_INFO(`MONITOR_NAME, "Checking \"j_type\" instruction")
+  endtask : check_j_type
+
+  //###############################################################################################
+  // Task: check_r_type
+  // Description: Specialized check for R-type instructions. This task is called when an R-type
+  //              instruction is detected.
+  //
+  // Example: R-type instructions include ADD, SUB, AND, etc.
+  //###############################################################################################
   task check_r_type();
     `PRINT_INFO(`MONITOR_NAME, "Checking \"r_type\" instruction")
   endtask : check_r_type
+
+  //###############################################################################################
+  // Task: check_s_type
+  // Description: Specialized check for S-type instructions. This task is called when an S-type
+  //              instruction is detected.
+  //
+  //              S-type instructions are typically used for store operations, such as storing a
+  //              a register value to memory. These instructions include store commands like SW
+  //              (store word), SH (store halfword), and SB (store byte).
+  //
+  // Example: S-type instructions calculate a memory address from a base register and an immediate
+  //          value, then store a value from a source register to that address.
+  //###############################################################################################
+  task check_s_type();
+    `PRINT_INFO(`MONITOR_NAME, "Checking \"s_type\" instruction")
+  endtask : check_s_type
+
+  //###############################################################################################
+  // Task: check_u_type
+  // Description: Specialized check for U-type instructions. This task is called when a U-type
+  //              instruction is detected.
+  //
+  //              U-type instructions are used for operations that require a 20-bit immediate value
+  //              that is sign-extended to 32 bits. The most common U-type instructions in RISC-V
+  //              are LUI (Load Upper Immediate) and AUIPC (Add Upper Immediate to PC).
+  //
+  // Example: U-type instructions are often used to set up immediate values in higher-order bits,
+  //          which can then be combined with lower order bits using other instructions for address
+  //          calculations.
+  //
+  //###############################################################################################
+  task check_u_type();
+    `PRINT_INFO(`MONITOR_NAME, "Checking \"u_type\" instruction")
+  endtask : check_u_type
 endclass : monitor
