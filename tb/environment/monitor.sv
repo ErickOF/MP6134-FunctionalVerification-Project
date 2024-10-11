57b5f84d27ec2dcab06769d735ae366f2d394460
fa-jimenez <najimenezch@gmail.com>
2024-10-10 19:33:14 -0600

    add monitor functionality

--- tb/environment/monitor.sv
+++ tb/environment/monitor.sv
@@ -14,5 +14,16 @@ class monitor;
 
   // Check task: Continuously monitors the read enable signal and checks the data output
   task check();
+    logic [31:0] instruction_data;
+    logic [31:0] input_data;
+    forever begin
+      @ (posedge intf.CLK);
+      if (!intf.HLT && !intf.RES) begin
+        instruction_data = intf.IDATA;
+        input_data = intf.DATAI;
+
+        $display("Time: %0t, Instruction IDATA: %h, Input Data: %h", $time, instruction_data, input_data);
+      end    
+    end
   endtask : check
 endclass : monitor
