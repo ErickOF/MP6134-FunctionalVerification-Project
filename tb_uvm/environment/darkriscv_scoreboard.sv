3d6efbe748ae7729296a3b4a3af167e48bdbbe92
jgavillalobos <jgavillalobos@gmail.com>
2024-10-21 22:11:49 -0600

    Fix scoreboard not included before

--- tb_uvm/environment/darkriscv_scoreboard.sv
+++ tb_uvm/environment/darkriscv_scoreboard.sv
@@ -11,9 +11,41 @@
 // The scoreboard does not contain any specific comparison logic yet, but the foundation is laid
 // out for handling the transactions received from the driver and monitor.
 //-------------------------------------------------------------------------------------------------
-`uvm_analysis_imp_decl(_drv)
-`uvm_analysis_imp_decl(_mon)
-class darkriscv_scoreboard extends uvm_scoreboard;
+
+`ifndef _DARKRISCV_SCOREBOARD_SV_
+`define _DARKRISCV_SCOREBOARD_SV_
+
+`uvm_analysis_imp_decl(_exp)
+`uvm_analysis_imp_decl(_act)
+
+class darkriscv_scoreboard #(type T = uvm_object) extends uvm_scoreboard;
+
+  //-----------------------------------------------------------------------------------------------
+  // Analysis Implementation: driscv_drv
+  //
+  // This is the analysis implementation port for receiving transactions from the expected item. It
+  // connected the component that will predict the expected value via the analysis port in the 
+  // testbench and will receive transactions of type T for comparison in the scoreboard.
+  //-----------------------------------------------------------------------------------------------
+  uvm_analysis_imp_exp #(T, riscv_scoreboard) expected_ap;
+
+  //-----------------------------------------------------------------------------------------------
+  // Analysis Implementation: driscv_mon
+  //
+  // This is the analysis implementation port for receiving transactions from the actual items from
+  // the monitor. It will collect observed transactions from the monitor via its analysis port for
+  // comparison in the scoreboard.
+  //-----------------------------------------------------------------------------------------------
+  uvm_analysis_imp_act #(T, riscv_scoreboard) actual_ap;
+
+  mailbox #(T) expected_mb;
+  mailbox #(T) actual_mb;
+
+  int unsigned match_count;
+  int unsigned mismatch_count;
+
+  bit objection_raised;
+
   `uvm_component_utils(darkriscv_scoreboard)
 
   //-----------------------------------------------------------------------------------------------
@@ -28,25 +60,15 @@ class darkriscv_scoreboard extends uvm_scoreboard;
   //-----------------------------------------------------------------------------------------------
   function new(string name="darkriscv_scoreboard", uvm_component parent=null);
     super.new(name, parent); // Call the base class constructor.
+
+    expected_mb = new();
+    actual_mb = new();
+
+    match_count = 0;
+    mismatch_count = 0;
+
+    objection_raised = 1'b0;
   endfunction : new
-
-  //-----------------------------------------------------------------------------------------------
-  // Analysis Implementation: driscv_drv
-  //
-  // This is the analysis implementation port for receiving transactions from the driver. It is 
-  // connected to the driver via the analysis port in the testbench and will receive darkriscv_item
-  // transactions for comparison in the scoreboard.
-  //-----------------------------------------------------------------------------------------------
-  uvm_analysis_imp_drv #(darkriscv_item, darkriscv_scoreboard) driscv_drv;
-
-  //-----------------------------------------------------------------------------------------------
-  // Analysis Implementation: driscv_mon
-  //
-  // This is the analysis implementation port for receiving transactions from the monitor. It will
-  // collect observed transactions from the monitor via its analysis port for comparison in the
-  // scoreboard.
-  //-----------------------------------------------------------------------------------------------
-  uvm_analysis_imp_mon #(darkriscv_item, darkriscv_scoreboard) driscv_mon;
 
   //-----------------------------------------------------------------------------------------------
   // Function: build_phase
@@ -60,8 +82,8 @@ class darkriscv_scoreboard extends uvm_scoreboard;
   //-----------------------------------------------------------------------------------------------
   function void build_phase(uvm_phase phase);
     // Initialize the analysis implementation ports for receiving transactions.
-    driscv_drv = new("driscv_drv", this);
-    driscv_mon = new("driscv_mon", this);
+    expected_ap = new("expected_ap", this);
+    actual_ap = new("actual_ap", this);;
   endfunction : build_phase
 
   //-----------------------------------------------------------------------------------------------
@@ -74,25 +96,72 @@ class darkriscv_scoreboard extends uvm_scoreboard;
   // - phase: Current UVM phase.
   //-----------------------------------------------------------------------------------------------
   virtual task run_phase(uvm_phase phase);
-    // No specific run-phase logic implemented yet.
+    T expected_data;
+    T actual_data;
+
+    forever begin
+      fork
+        begin
+          fork
+            begin
+              expected_mb.peek(expected_data);
+              handle_objetions(phase);
+            end
+            begin
+              actual_mb.peek(actual_data);
+              handle_objetions(phase);
+            end
+          join_any
+          disable fork;
+        end
+      join
+
+      expected_mb.get(expected_data);
+      actual_mb.get(actual_data);
+
+      if (expected_data.compare(actual_data)) begin
+        `uvm_info(get_type_name(), $sformatf("Data matched in scoreboard with expected = %s and actual = %s!", expected_data.sprint(), actual_data.sprint()), UVM_MEDIUM);
+        match_count++;
+      end
+      else begin
+        `uvm_error(get_type_name(), $sformatf("Data mismatched in scoreboard with expected = %s and actual = %s!", expected_data.sprint(), actual_data.sprint()));
+        mismatch_count++;
+      end
+
+      handle_objetions(phase);
+    end
   endtask : run_phase
 
-  //-----------------------------------------------------------------------------------------------
-  // Function: check_phase
-  //
-  // This function is part of the UVM check phase. It is currently empty but can be used to perform 
-  // checks and comparisons between the transactions received from the driver and monitor.
-  //
-  // Parameters:
-  // - phase: Current UVM phase.
-  //-----------------------------------------------------------------------------------------------
-  virtual function void check_phase(uvm_phase phase);
-    // No specific check logic implemented yet.
-  endfunction : check_phase
+  function void write_exp(T expected_item);
+    T expected_item_tmp;
 
-  virtual function void write_drv(darkriscv_item trans);
-  endfunction
+    if (!$cast(expected_item_tmp, expected_item.clone())) begin
+      `uvm_fatal(get_type_name(), "Failed to cast expected_item!")
+    end
 
-  virtual function void write_mon(darkriscv_item trans);
-  endfunction
+    expected_mb.try_put(expected_item_tmp);
+  endfunction : write_exp
+
+  function void write_act(T actual_item);
+    T actual_item_tmp;
+
+    if (!$cast(actual_item_tmp, actual_item.clone())) begin
+      `uvm_fatal(get_type_name(), "Failed to cast actual_item!")
+    end
+
+    actual_mb.try_put(actual_item_tmp);
+  endfunction : write_act
+
+  function void report_phase(uvm_phase phase);
+    phase.raise_objection(this, "Reporting results")
+
+    `uvm_info(get_type_name(), $sformatf("Scoreboard finished with %0d matches and %0d mismatches!", match_count, mismatch_count), UVM_NONE);
+    `uvm_info(get_type_name(), $sformatf("Scoreboard finished with %0d expected items waiting to be processed!", expected_mb.num()), UVM_NONE);
+    `uvm_info(get_type_name(), $sformatf("Scoreboard finished with %0d actual items waiting to be processed!", actual_mb.num()), UVM_NONE);
+
+    phase.lower_objection(this, "Reporting results finished")
+  endfunction : report_phase
+
 endclass : darkriscv_scoreboard
+
+`endif // _DARKRISCV_SCOREBOARD_SV_
